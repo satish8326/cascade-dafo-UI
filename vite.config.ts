e384fdd262f91931db1d759f09533d7c49742781
satish8326 <110229480+satish8326@users.noreply.github.com>
2026-01-16 17:41:06 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),mkcert()],
+  
   server: {
-    port: 3000
+    https: true, // HTTPS is required for Azure Portal SPA redirect URIs
+    port: 3000,
+    strictPort: false, // Allow other ports if 3001 is in use
+    host: true, // Allow external connections
   }
 });
